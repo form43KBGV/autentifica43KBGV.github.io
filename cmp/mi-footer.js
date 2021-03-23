516497b2c8a0a480aa4ca9b1c9b0316830e39027
form43KBGV <81197167+form43KBGV@users.noreply.github.com>
2021-03-22 18:02:12 -0600

    Delete cmp directory

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = /* html */
-      `Copyright &copy; 2021 Gutierrez Vargas Kevin Brayan`;
-  }
-}
-customElements.define("mi-footer", MiFooter);
