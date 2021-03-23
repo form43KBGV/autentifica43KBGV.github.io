e178e197f9908648e49bb0d04becab7d0ae7538b
form43KBGV <81197167+form43KBGV@users.noreply.github.com>
2021-03-22 18:04:11 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyA0eItZg_xZzAhjML3w7Li1zPXxbCUHqk8",
+    authDomain: "autentifica43.firebaseapp.com",
+    projectId: "autentifica43",
+    storageBucket: "autentifica43.appspot.com",
+    messagingSenderId: "587803819098",
+    appId: "1:587803819098:web:adb2faec95942b64392c63",
+    measurementId: "G-CMTB5FT43S"
+    });
